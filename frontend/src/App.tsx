0b501fd7847c15964d88472aca6229f49e4ee64a
notduyly <162235323+notduyly@users.noreply.github.com>
2025-06-05 13:34:45 -0700

    feat: added router to the officers page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,16 @@
-import { Header } from './components/Header'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { Header } from './components/header/Header'
+import { Officers } from './components/body/Officers'
 
 export const App = () => {
 
   return (
-    <>
+    <BrowserRouter>
       <Header />
-    </>
+      <Routes>
+        <Route path='/' />
+        <Route path='/officers' element={<Officers/>} />
+      </Routes>
+    </BrowserRouter>
   )
 }
---
+++ frontend/src/components/body/Officers.tsx
@@ -0,0 +1,9 @@
+
+export const Officers = () => {
+  return (
+    <div className="container mt-4">
+      <h1>Officers</h1>
+      <p>Meet the team behind SWE Academy!</p>
+    </div>
+  );
+};
