56ff7533feaa34b4f6c7f987fbc3bf37062a4f09
notduyly <162235323+notduyly@users.noreply.github.com>
2025-06-07 16:16:28 -0700

    feat: a footer with a responsive discord icon

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Header } from './components/Header';
 import { Officers } from './components/officer/Officers';
 import { Home } from './components/home/Home';
+import { Footer } from './components/Footer';
 
 export const App = () => {
 
@@ -12,6 +13,7 @@ export const App = () => {
         <Route path='/' element={<Home />}/>
         <Route path='/officers' element={<Officers/>} />
       </Routes>
+      <Footer />
     </BrowserRouter>
   );
 };
---
+++ frontend/src/components/Footer.tsx
@@ -0,0 +1,21 @@
+import { Navbar, Container } from "react-bootstrap";
+import { FaDiscord } from 'react-icons/fa';
+import './officer/OfficerCard.css';
+
+export const Footer = () => {
+  return (
+    <footer>
+      <Navbar bg="offwhite" variant="light" expand="lg" className="header-container">
+        <Container className="d-flex flex-column align-items-center justify-content-center pt-2 pb-0">
+          <a href="https://discord.gg/sSzzyJrHK3" target="_blank" rel="noopener noreferrer" className="social-icon">
+            <FaDiscord size={30} color="#5865F2" />
+          </a>
+
+          <Navbar.Text className="mt-3 small text-dark">
+            © swe_academy 2025
+          </Navbar.Text>
+        </Container>
+      </Navbar>
+    </footer>
+  );
+}; 
