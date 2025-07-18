f12a9be76df1c45e35a4b877775968297e94f503
notduyly <162235323+notduyly@users.noreply.github.com>
2025-07-18 15:18:31 -0700

    feat: add githired linkedin to footer

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -1,5 +1,5 @@
 import { Navbar, Container } from "react-bootstrap";
-import { FaDiscord } from 'react-icons/fa';
+import { FaDiscord, FaLinkedin } from 'react-icons/fa';
 import './officer/OfficerCard.css';
 
 export const Footer = () => {
@@ -8,13 +8,20 @@ export const Footer = () => {
     <footer>
       <Navbar bg="offwhite" variant="light" expand="lg" className="header-container">
         <Container className="d-flex flex-column align-items-center justify-content-center pt-2 pb-0">
-          <a href="https://discord.gg/sSzzyJrHK3" target="_blank" rel="noopener noreferrer" className="social-icon">
-            <FaDiscord size={30} color="#5865F2" />
-          </a>
+          
+          <div className="d-flex flex-row gap-3">
+            <a href="https://discord.gg/sSzzyJrHK3" target="_blank" rel="noopener noreferrer" className="social-icon">
+              <FaDiscord size={30} color="#5865F2" />
+            </a>
+            <a href="https://www.linkedin.com/company/githired-uwt/" target="_blank" rel="noopener noreferrer" className="social-icon">
+              <FaLinkedin size={30} color="#5865F2" />
+            </a>
+          </div>
 
-          <Navbar.Text className="mt-3 small text-dark">
+          <Navbar.Text className="mt-1 small text-dark">
             © GitHired {new Date().getFullYear()}
           </Navbar.Text>
+
         </Container>
       </Navbar>
     </footer>
