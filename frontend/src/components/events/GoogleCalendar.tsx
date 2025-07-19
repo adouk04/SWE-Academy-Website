5a81560201a810200499314eab651b1c48d9ef85
notduyly <162235323+notduyly@users.noreply.github.com>
2025-07-19 16:08:47 -0700

    fix: google calendar now sync with githired email

--- frontend/src/components/events/GoogleCalendar.tsx
+++ frontend/src/components/events/GoogleCalendar.tsx
@@ -4,11 +4,11 @@ import "./googleCalendar.css"
 export const GoogleCalendar = () => {
     return (
                 <div className="calendar-wrapper">
-                    <iframe
-                        title="Club Google Calendar"
-                        src="https://calendar.google.com/calendar/embed?height=600&wkst=1&ctz=America%2FLos_Angeles&showPrint=0&showTz=0&showCalendars=0&src=MGEzY2EwOWU4ZmNjMmE4NjEyNGExMzVmNmU2OGQwZWZlNDM0YjhhMjFhYTcxMzJiNDFkZDJhNDhmYjViYjdlNEBncm91cC5jYWxlbmRhci5nb29nbGUuY29t&color=%23000000"
-                        className="iframecal"
-                    ></iframe>
+                    <iframe 
+                        src="https://calendar.google.com/calendar/embed?height=600&wkst=1&ctz=America%2FLos_Angeles&showPrint=0&src=Z2l0aGlyZWQyNEBnbWFpbC5jb20&color=%23039be5" 
+                        width="590" 
+                        height="600">
+                    </iframe>
                 </div>
     );
 };
